e6c04ea8dcf6e4ef33c36799f963d410b27e2ba3
Arturo Tagle <arturo@kunder.cl>
2019-06-16 18:15:31 -0400

    fix death star films

--- scripts/download.js
+++ scripts/download.js
@@ -80,6 +80,30 @@ const a1 = {
   url: 'https://swapi.co/api/people/91/' 
 }
 
+const ds = {
+  'name': 'Death Star',
+  'model': 'DS-1 Orbital Battle Station',
+  'manufacturer': 'Imperial Department of Military Research, Sienar Fleet Systems',
+  'cost_in_credits': '1000000000000',
+  'length': '120000',
+  'max_atmosphering_speed': 'n/a',
+  'crew': '342953',
+  'passengers': '843342',
+  'cargo_capacity': '1000000000000',
+  'consumables': '3 years',
+  'hyperdrive_rating': '4.0',
+  'MGLT': '10',
+  'starship_class': 'Deep Space Mobile Battlestation',
+  'pilots': [],
+  'films': [
+    'https://swapi.co/api/films/1/',
+    integraFilm.url
+  ],
+  'created': '2014-12-10T16:36:50.509000Z',
+  'edited': '2014-12-22T17:35:44.452589Z',
+  'url': 'https://swapi.co/api/starships/9/'
+}
+
 function normalizeUrl(url) {
   return new URL(url).toString();
 }
@@ -109,6 +133,12 @@ async function cacheResources() {
         if(!data.previous) {
           data.results.push(prof);
           data.results.push(a1);
+        }
+      } else if(name === 'starships') {
+        for(var i = 0; i < data.results.length; i++) {
+          if(data.results[i].url === ds.url) {
+            data.results[i] = ds;
+          }
         }
       }
 
